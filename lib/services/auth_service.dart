ad04c9617f1a5338fbdb810c1afcdeeaaadf2454
Raf <raf.vermeylen@student.kdg.be>
2026-04-07 21:42:30 +0200

    chat works, bugs need to be fixed. Admin username needs to be fixed.

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -10,10 +10,16 @@
    file needs to change.
 */
 
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class AuthService {
   AuthService._();
 
   static final AuthService instance = AuthService._();
+
+  static const _projectId = 'radio-apollo-90693';
+  static const _region = 'europe-west1';
 
   String _role = 'user';
 
@@ -21,8 +27,28 @@ class AuthService {
 
   bool get isAdmin => _role == 'admin';
 
-  void login(String password) {
-    if (password == 'apollo123') _role = 'admin';
+  Future<void> login(String password) async {
+    try {
+      final uri = Uri.parse(
+        'https://$_region-$_projectId.cloudfunctions.net/adminLogin',
+      );
+
+      final response = await http.post(
+        uri,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'data': {'password': password}}),
+      );
+
+      if (response.statusCode == 200) {
+        _role = 'admin';
+      } else {
+        _role = 'user';
+        throw Exception('Invalid password');
+      }
+    } catch (e) {
+      _role = 'user';
+      rethrow;
+    }
   }
 
   void logout() => _role = 'user';
